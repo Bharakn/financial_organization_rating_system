d8a58c19275f271e109e2a7fe7cc6adcc0db1928
Venkysvry <87991111+Venkysvry@users.noreply.github.com>
2022-05-27 15:36:36 +0530

    venkateshcheck

--- financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/FinanceTeamService.java
+++ financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/FinanceTeamService.java
@@ -1,5 +1,10 @@
 package com.cg.financial_organization_rating_system.services;
 
+
+
+import com.cg.financial_organization_rating_system.entities.FinanceTeam;
+
 public interface FinanceTeamService {
+	public int addMem(FinanceTeam finance);
 
 }
--- financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/FinanceTeamServiceImpl.java
+++ financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/FinanceTeamServiceImpl.java
@@ -1,8 +1,8 @@
 package com.cg.financial_organization_rating_system.services;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.financial_organization_rating_system.entities.FinanceTeam;
 import com.cg.financial_organization_rating_system.repository.FinanceTeamRepository;
 
 @Service
@@ -10,5 +10,16 @@ public class FinanceTeamServiceImpl implements FinanceTeamService {
 	
 	@Autowired
 	FinanceTeamRepository financerepo;
+
+	
+
+	@Override
+	public int addMem(FinanceTeam finance) {
+		FinanceTeam financef=new FinanceTeam();
+		financef.setAdminId(finance.getAdminId());
+		financef.setPassword(finance.getPassword());
+		financerepo.save(financef);
+		return finance.getSlno();
+	}
 
 }
