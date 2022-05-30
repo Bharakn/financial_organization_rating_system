212755bd856551d65423e53203820887e5573384
Bharakn <103725471+Bharakn@users.noreply.github.com>
2022-05-30 20:57:54 +0530

    bharath2

--- financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/OrganizationRepService.java
+++ financial_organization_rating_system/lib/src/main/java/com/cg/financial_organization_rating_system/services/OrganizationRepService.java
@@ -1,11 +1,7 @@
 package com.cg.financial_organization_rating_system.services;
 
-import java.util.Optional;
-
-import com.cg.financial_organization_rating_system.dto.OrgRepLoginDto;
 import com.cg.financial_organization_rating_system.dto.OrganizationRepRegistrationDto;
 import com.cg.financial_organization_rating_system.dto.OrganizationRepUpdateDetailsDto;
-import com.cg.financial_organization_rating_system.entities.LoginDetails;
 
 public interface OrganizationRepService {
     int addOrganizationRep(OrganizationRepRegistrationDto orgrepdto);
